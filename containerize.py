b3e1e87bb6f591addc8d455fb928f34a6ad66e2e
meshde <meshde.md@gmail.com>
2018-06-19 03:38:01 +0000

    Workaround for #2

--- containerize.py
+++ containerize.py
@@ -1,7 +1,9 @@
+import gc
 import os
 from pyLinux import linux
 from pyLinux.cgroups import Cgroup
 from pyLinux import utils
+import time
 import tarfile
 import traceback
 import uuid
@@ -19,25 +21,43 @@ def isolate_resources():
     return
 
 def extract(tarf, root):
-    print('Here')
     with tarfile.open(tarf) as t:
-        print('Here now')
-        # members  = [m for m in t.getmembers() if m.type not in
-        #             (tarfile.CHRTYPE, tarfile.BLKTYPE)]
-        print('Here this time')
-        # t.extractall(root, members=members)
-        for member in iter(t.getmembers()):
-            print('Extracting {}'.format(member))
-            t.extract(member, path=root)
+        members  = [m for m in t.getmembers() if m.type not in
+                    (tarfile.CHRTYPE, tarfile.BLKTYPE)]
+        t.extractall(root, members=members)
     return
 
-def get_container_root():
+def extract2(tarf, root):
+    with tarfile.open(tarf) as t:
+        print('Here')
+        member = t.next()
+        i = 1
+        while member:
+            print(member.name, member.size, hex(id(member)), member.type)
+            if member.type not in (tarfile.CHRTYPE, tarfile.BLKTYPE):
+                try:
+                    t.extract(member, root)
+                    print('Extraction working')
+                except OSError:
+                    print('This is it mofo')
+                    pass
+            del member
+            print('Delete')
+            gc.collect()
+            print('Collection')
+            member = t.next()
+    return
+
+def get_container_root(files='all'):
     container_id = str(uuid.uuid4())
     container_root = os.path.join(CONTAINER_DIR, container_id)
     os.makedirs(container_root)
 
-    print('--> Extracting File to Root...')
-    extract('ubuntu.tar', container_root)
+    print('--> Extracting Files to Root...')
+    if files == 'all':
+        extract('ubuntu.tar', container_root)
+    else:
+        extract(files, container_root)
     return container_root
 
 def mount_files(root):
@@ -75,7 +95,7 @@ def isolate_filesystem():
     chroot(root)
     return
 
-def contain(cmd):
+def contain_clone(cmd):
     # print('Isolating Resources...')
     # isolate_resources()
     cmd = utils.get_object_from_pointer(cmd)
@@ -87,11 +107,31 @@ def contain(cmd):
     os.execv(cmd[0], cmd)
     return
 
-def run():
+def run_clone():
     COMMAND = '/bin/sh'
-    pid = linux.clone(contain, args=COMMAND)
+    print('OLO')
+    pid = linux.clone(contain, stack_size=int(32768), args=COMMAND)
+    print('LOLO')
     status = linux.waitpid(pid)
     print('{0} exited with status {1}'.format(pid, status))
 
+def contain_fork(cmd):
+    # isolate_namespaces()
+    # isolate_resources()
+    cmd = cmd.split()
+    isolate_filesystem()
+    os.execvp(cmd[0], cmd)
+    return
+
+def run_fork():
+    CMD = '/bin/bash'
+    pid = os.fork()
+    if pid == 0:
+        contain_fork(CMD)
+    else:
+        time.sleep(5)
+        _, status = os.waitpid(pid, 0)
+    print('{0} returned with status: {1}'.format(pid, status))
+
 if __name__ == '__main__':
-    run()
+    run_fork()
